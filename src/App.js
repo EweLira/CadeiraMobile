0af24b4991667135130bb6067ca1793a6122e896
Ewellyn Lira <113145267+EweLira@users.noreply.github.com>
2023-09-06 20:18:20 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,29 +1,64 @@
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+function ApiConsumptionComponent() {
+    const [userResult, setUserResult] = useState(null);
+    const [jobResult, setJobResult] = useState(null);
+    const [pokemonResult, setPokemonResult] = useState(null);
+
+    const fetchUser = () => {
+        fetch('https://jsonplaceholder.typicode.com/users/1')
+            .then(response => response.json())
+            .then(data => {
+                setUserResult(JSON.stringify(data));
+            });
+    };
+
+    const fetchJob = () => {
+        fetch('https://reqres.in/api/users/1')
+            .then(response => response.json())
+            .then(data => {
+                setJobResult(JSON.stringify(data));
+            });
+    };
+
+    const fetchPokemon = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon/1')
+            .then(response => response.json())
+            .then(data => {
+                setPokemonResult(JSON.stringify(data));
+            });
+    };
+
+    return (
+        <div className="container mt-5">
+            <h1 className="mb-4">Consultas de API</h1>
+
+            <button className="btn btn-primary me-3" onClick={fetchUser}>Obter Usuário</button>
+            <button className="btn btn-success me-3" onClick={fetchJob}>Obter Emprego</button>
+            <button className="btn btn-info" onClick={fetchPokemon}>Obter Pokémon</button>
+
+            {userResult && (
+                <div id="userResult" className="mt-4">
+                    <h2>Resultado do Usuário</h2>
+                    <pre className="bg-light p-3">{userResult}</pre>
+                </div>
+            )}
+            
+            {jobResult && (
+                <div id="jobResult" className="mt-4">
+                    <h2>Resultado do Emprego</h2>
+                    <pre className="bg-light p-3">{jobResult}</pre>
+                </div>
+            )}
+            
+            {pokemonResult && (
+                <div id="pokemonResult" className="mt-4">
+                    <h2>Resultado do Pokémon</h2>
+                    <pre className="bg-light p-3">{pokemonResult}</pre>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default App;
+export default ApiConsumptionComponent;
